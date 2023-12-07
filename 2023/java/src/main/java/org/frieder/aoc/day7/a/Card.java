9ff3c7670b99094be3582d8bd8f116326fe82c92
Frieder <frieder.reinhold@exxeta.com>
2023-12-07 23:47:57 +0100

    Day 7 2023 more Java Streams

--- 2023/java/src/main/java/org/frieder/aoc/day7/a/Card.java
+++ 2023/java/src/main/java/org/frieder/aoc/day7/a/Card.java
@@ -3,10 +3,12 @@ package org.frieder.aoc.day7.a;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.frieder.aoc.day7.a.Card.CardValue.FIVE_OF_A_KIND;
 import static org.frieder.aoc.day7.a.Card.CardValue.FOUR_OF_A_KIND;
@@ -20,47 +22,44 @@ import static org.frieder.aoc.day7.a.Card.CardValue.TWO_PAIR;
 @Getter
 public class Card implements Comparable<Card> {
 
+    enum CardValue {
+        HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND
+    }
+
     private String hand;
 
     private Integer bid;
 
-    enum CardValue {
-        HIGH_CARD,
-        ONE_PAIR,
-        TWO_PAIR,
-        THREE_OF_A_KIND,
-        FULL_HOUSE,
-        FOUR_OF_A_KIND,
-        FIVE_OF_A_KIND
-    }
+    private static final Map<Character, Character> CHAR_MAPPING = new HashMap<>() {{
+        put('A', 'E');
+        put('K', 'D');
+        put('Q', 'C');
+        put('J', 'B');
+        put('T', 'A');
+    }};
+
+    private static final Map<Integer, Function<Map<Character, Long>, CardValue>> TYPE_DETECTORS = new HashMap<>() {{
+        put(1, m -> FIVE_OF_A_KIND);
+        put(2, m -> m.containsValue(4L) ? FOUR_OF_A_KIND : FULL_HOUSE);
+        put(3, m -> m.containsValue(3L) ? THREE_OF_A_KIND : TWO_PAIR);
+        put(4, m -> ONE_PAIR);
+        put(5, m -> HIGH_CARD);
+    }};
 
     private CardValue getCardValue() {
-        Map<Character, Long> counts = IntStream.range(0, hand.length())
-                .mapToObj(hand::charAt)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-        switch (counts.size()) {
-            case 1:
-                return FIVE_OF_A_KIND;
-            case 2:
-                return counts.containsValue(4L) ? FOUR_OF_A_KIND : FULL_HOUSE;
-            case 3:
-                return counts.containsValue(3L) ? THREE_OF_A_KIND : TWO_PAIR;
-            case 4:
-                return ONE_PAIR;
-            case 5:
-                return HIGH_CARD;
-        }
-
-        throw new RuntimeException();
+        return Stream.of(IntStream.range(0, hand.length())
+                        .mapToObj(hand::charAt)
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
+                .map(counts -> TYPE_DETECTORS.get(counts.size()).apply(counts))
+                .findFirst()
+                .orElseThrow();
     }
 
     private String getHexHand() {
-        return hand.replaceAll("A", "E")
-                .replaceAll("K", "D")
-                .replaceAll("Q", "C")
-                .replaceAll("J", "B")
-                .replaceAll("T", "A");
+        return hand.chars()
+                .map(ch -> CHAR_MAPPING.containsKey((char) ch) ? CHAR_MAPPING.get((char) ch) : ch)
+                .collect(StringBuilder::new, (builder, c) -> builder.append((char) c), StringBuilder::append)
+                .toString();
     }
 
     private int compareHand(Card other) {
@@ -75,15 +74,9 @@ public class Card implements Comparable<Card> {
 
     @Override
     public int compareTo(Card other) {
-        CardValue v1 = getCardValue();
-        CardValue v2 = other.getCardValue();
-
-        if (v1.compareTo(v2) < 0)
-            return -1;
-
-        if (v1.compareTo(v2) > 0)
-            return 1;
-
-        return compareHand(other);
+        return Stream.of(getCardValue().compareTo(other.getCardValue()))
+                .map(r -> r != 0 ? r : compareHand(other))
+                .findFirst()
+                .orElse(0);
     }
 }
--- 2023/java/src/main/java/org/frieder/aoc/day7/b/Card.java
+++ 2023/java/src/main/java/org/frieder/aoc/day7/b/Card.java
@@ -3,10 +3,12 @@ package org.frieder.aoc.day7.b;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.frieder.aoc.day7.b.Card.CardValue.FIVE_OF_A_KIND;
 import static org.frieder.aoc.day7.b.Card.CardValue.FOUR_OF_A_KIND;
@@ -20,67 +22,62 @@ import static org.frieder.aoc.day7.b.Card.CardValue.TWO_PAIR;
 @Getter
 public class Card implements Comparable<Card> {
 
+    enum CardValue {
+        HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND
+    }
+
     private String hand;
 
     private Integer bid;
 
-    enum CardValue {
-        HIGH_CARD,
-        ONE_PAIR,
-        TWO_PAIR,
-        THREE_OF_A_KIND,
-        FULL_HOUSE,
-        FOUR_OF_A_KIND,
-        FIVE_OF_A_KIND
-    }
+    private static final Map<Character, Character> CHAR_MAPPING = new HashMap<>() {{
+        put('A', 'E');
+        put('K', 'D');
+        put('Q', 'C');
+        put('J', '1');
+        put('T', 'A');
+    }};
+
+    private static final Map<Integer, Function<Map<Character, Long>, CardValue>> TYPE_DETECTORS = new HashMap<>() {{
+        put(1, m -> FIVE_OF_A_KIND);
+        put(2, m -> m.containsValue(4L) ?
+                ((m.containsKey('J') && (m.get('J') == 1 || m.get('J') == 4)) ?
+                        FIVE_OF_A_KIND :
+                        FOUR_OF_A_KIND) :
+                ((m.containsKey('J') && (m.get('J') == 2 || m.get('J') == 3)) ?
+                        FIVE_OF_A_KIND :
+                        FULL_HOUSE));
+        put(3, m -> m.containsValue(3L) ?
+                ((m.containsKey('J') && (m.get('J') == 1 || m.get('J') == 3)) ?
+                        FOUR_OF_A_KIND :
+                        THREE_OF_A_KIND) :
+                ((m.containsKey('J') && (m.get('J') == 2)) ?
+                        FOUR_OF_A_KIND :
+                        ((m.containsKey('J') && m.get('J') == 1) ?
+                                FULL_HOUSE :
+                                TWO_PAIR)));
+        put(4, m -> (m.containsKey('J') && (m.get('J') == 1 || m.get('J') == 2)) ?
+                THREE_OF_A_KIND :
+                ONE_PAIR);
+        put(5, m -> (m.containsKey('J') && m.get('J') == 1) ?
+                ONE_PAIR :
+                HIGH_CARD);
+    }};
 
     private CardValue getCardValue() {
-        Map<Character, Long> counts = IntStream.range(0, hand.length())
-                .mapToObj(hand::charAt)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-        switch (counts.size()) {
-            case 1: // 5
-                return FIVE_OF_A_KIND;
-            case 2:
-                if (counts.containsValue(4L)) // 4:1
-                    return (counts.containsKey('J') && (counts.get('J') == 1 || counts.get('J') == 4)) ?
-                            FIVE_OF_A_KIND :
-                            FOUR_OF_A_KIND;
-                else // 3:2
-                    return (counts.containsKey('J') && (counts.get('J') == 2 || counts.get('J') == 3)) ?
-                            FIVE_OF_A_KIND :
-                            FULL_HOUSE;
-            case 3:
-                if (counts.containsValue(3L)) // 3:1:1
-                    return (counts.containsKey('J') && (counts.get('J') == 1 || counts.get('J') == 3)) ?
-                            FOUR_OF_A_KIND :
-                            THREE_OF_A_KIND;
-                else // 2:2:1
-                    return (counts.containsKey('J') && (counts.get('J') == 2)) ?
-                            FOUR_OF_A_KIND :
-                            ((counts.containsKey('J') && counts.get('J') == 1) ?
-                                    FULL_HOUSE :
-                                    TWO_PAIR);
-            case 4:
-                return (counts.containsKey('J') && (counts.get('J') == 1 || counts.get('J') == 2)) ?
-                        THREE_OF_A_KIND :
-                        ONE_PAIR;
-            case 5:
-                return (counts.containsKey('J') && counts.get('J') == 1) ?
-                        ONE_PAIR :
-                        HIGH_CARD;
-        }
-
-        throw new RuntimeException();
+        return Stream.of(IntStream.range(0, hand.length())
+                        .mapToObj(hand::charAt)
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())))
+                .map(counts -> TYPE_DETECTORS.get(counts.size()).apply(counts))
+                .findFirst()
+                .orElseThrow();
     }
 
     private String getHexHand() {
-        return hand.replaceAll("A", "E")
-                .replaceAll("K", "D")
-                .replaceAll("Q", "C")
-                .replaceAll("J", "1")
-                .replaceAll("T", "A");
+        return hand.chars()
+                .map(ch -> CHAR_MAPPING.containsKey((char) ch) ? CHAR_MAPPING.get((char) ch) : ch)
+                .collect(StringBuilder::new, (builder, c) -> builder.append((char) c), StringBuilder::append)
+                .toString();
     }
 
     private int compareHand(Card other) {
@@ -95,15 +92,9 @@ public class Card implements Comparable<Card> {
 
     @Override
     public int compareTo(Card other) {
-        CardValue v1 = getCardValue();
-        CardValue v2 = other.getCardValue();
-
-        if (v1.compareTo(v2) < 0)
-            return -1;
-
-        if (v1.compareTo(v2) > 0)
-            return 1;
-
-        return compareHand(other);
+        return Stream.of(getCardValue().compareTo(other.getCardValue()))
+                .map(r -> r != 0 ? r : compareHand(other))
+                .findFirst()
+                .orElse(0);
     }
 }
